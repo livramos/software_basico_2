8958523a6e6ee99b57b42434cc327b391c4444d9
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2026-03-09 09:45:08 -0300

    Refactor string2num function for base conversion

--- lab_2_5/03/inf1018/ex1_lab2.c
+++ lab_2_5/03/inf1018/ex1_lab2.c
@@ -1,18 +1,27 @@
+
 #include <ctype.h>
 #include <stdio.h>
 
-int string2num (char *s, int base) {
-  int a = 0;
- 
-  for (; *s; s++)
-    a = a*isdigit(base) + (*s - '0');
-  return a;
+int string2num(char *s, int base) {
+    int a = 0;
+    int dig;
+
+    for (; *s != '\0'; s++) {
+        if (isdigit(*s)) {
+            dig = *s - '0';
+        } else {
+            dig = tolower(*s) - 'a' + 10;
+        }
+
+        a = a * base + dig;
+    }
+
+    return a;
 }
 
-int main (void) {
-
-   printf("%d\n", string2num("1a", 16));
-  printf("%d\n", string2num("a09b", 16));
-  printf("%d\n", string2num("z09b", 36));
-  return 0;
+int main(void) {
+    printf("%d\n", string2num("1a", 16));
+    printf("%d\n", string2num("a09b", 16));
+    printf("%d\n", string2num("z09b", 36));
+    return 0;
 }
