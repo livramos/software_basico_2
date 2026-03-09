4e92a39e6cb8aadea2c5c244bd2a9df3ce0f4589
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2026-03-09 12:06:02 +0000

    lab2

---
+++ lab_2_5/03/inf1018/ex1_lab2.c
@@ -0,0 +1,18 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int string2num (char *s, int base) {
+  int a = 0;
+ 
+  for (; *s; s++)
+    a = a*isdigit(base) + (*s - '0');
+  return a;
+}
+
+int main (void) {
+
+   printf("%d\n", string2num("1a", 16));
+  printf("%d\n", string2num("a09b", 16));
+  printf("%d\n", string2num("z09b", 36));
+  return 0;
+}
