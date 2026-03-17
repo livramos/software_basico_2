e872b4585f75d13100596cf76e611bb80dcc34c3
Liv_Ramos <84927414+livramos@users.noreply.github.com>
2026-03-17 02:30:20 +0000

    lab_3 finalizado

--- lab_3_12/ex3.c
+++ lab_3_12/ex3.c
@@ -1,28 +1,26 @@
 #include <stdio.h>
 unsigned char switch_byte(unsigned char x){
     int fh= x & 0x0F;
-    printf("%x\n", fh);
+
     int sh= x & 0xF0;
-    printf("%x\n", sh);
+
     int result= sh>>4 | fh<<4; 
     return result;
 }
 unsigned char rotate_left(unsigned char x, int n){
-  /*int z= x << n;
+    int z= x << n;
     int y= x >> (8-n);
     int result= z | y;
-    return result;*/
-    for (int i=0; i<n; i++){
-        int z= x << 1;
-        int y= x >> (8-1);
-        x= z | y;
-    }
+    return result;
+
 
 }
 
 int main (void){
     unsigned char x=0xAB;
-    printf("%x\n", switch_byte(x));
+    
     printf("%x\n", rotate_left(0x61, 2));
+    printf("%x\n", rotate_left(0x61, 1));
+    printf("%x\n", rotate_left(0x61, 7));
     return 0;
 }
